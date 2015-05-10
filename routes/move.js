01e5da4016727e7b7304470b11dc61d4c4e824ae
EdikDolynskyi <dolynskiy.eduard@gmail.com>
2015-05-10 18:02:15 +0300

    Rewrite origin code and processing homework under the following problem

--- routes/move.js
+++ routes/move.js
@@ -27,6 +27,27 @@ router.post('/:id/:xCoordinate/:yCoordinate', function(req, res) {
 
     res.end(util.format('Ok, new point added to %s: (%s, %s)', man.name, point.x, point.y));
 });
+router.post('/:id/:xCoordinate/:yCoordinate', function(req, res) {
+    var currentGame = cg.get();
+    if (!currentGame)
+        return res.end('There no game');
+
+    var id = req.params.id;
+    var x = parseInt(req.params.xCoordinate);
+    var y = parseInt(req.params.yCoordinate);
+
+    if (isNaN(x) || isNaN(y))
+        return res.end('X and Y coordinates should be numbers');
+
+    var man = currentGame.findFighter(id);
+    if (!man)
+        return res.end('There is no fighter with id: ' + id);
+
+    var point = new Vector(x, y);
+    man.route.points.push(point);
+
+    res.end(util.format('Ok, new point added to %s: (%s, %s)', man.name, point.x, point.y));
+});
 
 
 module.exports = router;
